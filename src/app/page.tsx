cbf945bec462580953c3bf019f1793be96b9abc3
NishthaSharma-22 <nishthapinjore@gmail.com>
2026-02-05 13:26:55 +0530

    added homepage text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function HomePage() {
-  return <div className="h-20 w-90 bg-red-100 text-blue-200">HomePage</div>;
+  return <div className="h-20 w-90 bg-red-100 text-blue-200"><p>Homepage</p></div>;
 }
 
 export default HomePage;
